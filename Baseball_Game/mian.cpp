f30364258602fe05a02e33f5d42659803b99e2c5
snake <lcpdfk@163.com>
2020-07-23 21:55:06 +0800

    682. 棒球比赛

---
+++ Baseball_Game/mian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "solution.h"
+
+using namespace std;
+
+//682. ∞Ù«Ú±»»¸
+int main() {
+    Solution solution;
+    vector<string> scores = { "5","-2","4","C","D","9","+","+" };
+    cout << solution.calPoints(scores) << endl;;
+    system("pause");
+    return 0;
+}
---
+++ Baseball_Game/solution.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int calPoints(vector<string>& ops);
+};
