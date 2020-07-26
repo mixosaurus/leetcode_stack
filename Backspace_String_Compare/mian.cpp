1913b40357e0169ac8f50e146209a2324e30b898
snake <lcpdfk@163.com>
2020-07-26 16:08:02 +0800

    比较含退格的字符串

--- Backspace_String_Compare/mian.cpp
+++ Backspace_String_Compare/mian.cpp
@@ -7,8 +7,9 @@ using namespace std;
 //844. 比较含退格的字符串
 int main() {
     Solution solution;
-    vector<string> scores = { "5","-2","4","C","D","9","+","+" };
-    cout << solution.calPoints(scores) << endl;;
+    string s = "a##c";
+    string t = "#a#c";
+    cout << boolalpha << solution.backspaceCompare(s,t) << endl;;
     system("pause");
     return 0;
 }
--- Backspace_String_Compare/solution.cpp
+++ Backspace_String_Compare/solution.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 bool Solution::backspaceCompare(string S, string T) {
+    //原地算法，把原字符串作为栈
+    int top = -1;
+    for (char ch:S) {
+        if (ch !='#') {
+            S[++top] = ch;
+        } else if(ch=='#'&&top>-1) {
+            top--;
+        }
+    }
+    S.resize(top+1);
 
-    return false;
+    top = -1;
+    for (char ch:T) {
+        if (ch !='#') {
+            T[++top] = ch;
+        } else if(ch=='#'&&top>-1) {
+            top--;
+        }
+    }
+    T.resize(top+1);
+    return T == S;
 }
