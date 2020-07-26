342fb309e8f773895c46bcb0281c736d43a34c4c
snake <lcpdfk@163.com>
2020-07-26 14:37:18 +0800

    496. 下一个更大元素 I

--- Next_Greater_Element_I/mian.cpp
+++ Next_Greater_Element_I/mian.cpp
@@ -7,7 +7,13 @@ using namespace std;
 //496. 下一个更大元素 I
 int main() {
     Solution solution;
+    vector<int> nums1= { 4,1,2 };
+    vector<int> nums2= { 1,3,4,2 };
+    vector<int> num = solution.nextGreaterElement(nums1, nums2);
+    for (int n :num) {
+        cout << n<<" ";
+    }
+    cout<<endl;
     system("pause");
-
     return 0;
 }
--- Next_Greater_Element_I/solution.cpp
+++ Next_Greater_Element_I/solution.cpp
@@ -1,7 +1,30 @@
 #include "solution.h"
 #include<iostream>
+#include <unordered_map>
 using namespace std;
 
 vector<int> Solution::nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    return vector<int>();
+    vector<int> result;
+    int top = -1;
+    int stack[1000];
+    unordered_map<int, int> this_and_next;
+    for(int num2:nums2){
+        if (top==-1||num2<=stack[top]) {
+            stack[++top] = num2;
+        } else {
+            //将栈内所有小于num的元素出栈，并与num一起存入map
+            while (top>-1&&num2>stack[top]) {
+                this_and_next[stack[top--]] = num2;
+            }
+            stack[++top] = num2;
+        }
+    }
+    for (int num1 : nums1) {
+        if (this_and_next.find(num1) != this_and_next.end()) {
+            result.push_back(this_and_next[num1]);
+        } else {  
+            result.push_back(-1);
+        }
+    }
+    return result;
 }
